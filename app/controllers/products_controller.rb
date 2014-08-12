07759f1033a4a6c0c63fd2f52e3a0b12c0f70dcb
eivel <eivelloth@gmail.com>
2014-08-12 15:48:40 +0200

    Replaced Devise's .erb views with .haml views. Updated some views with Bootstrap.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :is_owner?, only: [:edit, :update]
+  before_action :is_owner?, only: [:edit, :update, :destroy]
 
   expose(:category)
   expose(:products)
@@ -22,6 +22,7 @@ class ProductsController < ApplicationController
 
   def create
     self.product = Product.new(product_params)
+    self.product.user = current_user
     if product.save
       category.products << product
       redirect_to category_product_url(category, product), notice: 'Product was successfully created.'
